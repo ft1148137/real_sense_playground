e5b4a1cbf12a5ef9eaa2002671a4a117a31d0fdb
ft1148137@gmail.com <ft1148137@gmail.com>
2019-12-21 14:15:41 +0100

    init

---
+++ realsense_test/CMakeFiles/realsense_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realsense_test.dir/realsense_test.cpp.o"
+  "realsense_test.pdb"
+  "realsense_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realsense_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ realsense_test/realsense_test.cpp
@@ -0,0 +1,9 @@
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include <iostream>             // for cout
+
+int main (int argc, char *argv[])
+{
+rs2::pipeline realsense_pip;
+pip.start();
+  return 0;
+}
