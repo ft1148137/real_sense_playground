3afbc55339d06e6507943d2901fa87878623878c
ft1148137@gmail.com <ft1148137@gmail.com>
2019-12-23 16:16:18 +0100

    keep working..

--- realsense_test/orb_match.cpp
+++ realsense_test/orb_match.cpp
@@ -11,29 +11,34 @@
 int main(int argc, char *argv[]){
 	rs2::pipeline realsense_pip;
 	rs2::config config;
-	std::cout<<"/n set_config";
-	config.enable_stream(RS2_STREAM_COLOR,640,480,RS2_FORMAT_BGR8,15);
-	config.enable_stream(RS2_STREAM_DEPTH,640,480,RS2_FORMAT_Z16,15);
-	config.enable_stream(RS2_STREAM_INFRARED,640,480,RS2_FORMAT_Y8,15);
+	config.enable_stream(RS2_STREAM_COLOR,640,480,RS2_FORMAT_BGR8,30);
+	//config.enable_stream(RS2_STREAM_DEPTH,640,480,RS2_FORMAT_Z16,15);
+	//config.enable_stream(RS2_STREAM_INFRARED,640,480,RS2_FORMAT_Y8,15);
+	
 
 	realsense_pip.start(config);
 	rs2::frameset frames;
 	std::vector<cv::cuda::GpuMat> Mat_buffer;
-
+	for(int i = 0; i<30;i++){
+					frames = realsense_pip.wait_for_frames();
+}
 	while(true){
 		for(int i = 0; i<2;i++){
 			frames = realsense_pip.wait_for_frames();
 			rs2::frame color_frame = frames.get_color_frame();
 			cv::Mat color(cv::Size(640,480),CV_8UC3,(void*)color_frame.get_data(),cv::Mat::AUTO_STEP);
-			cv::cuda::GpuMat color_now(color);
-			Mat_buffer.push_back(color_now);
+			cv::Mat wb_img;
+			cv::cvtColor(color,wb_img,cv::COLOR_BGR2GRAY);
+			cv::cuda::GpuMat wb_now(wb_img);
+			Mat_buffer.push_back(wb_now);
 		}
 		std::vector<cv::KeyPoint> keypoints_1, keypoints_2;
 		cv::cuda::GpuMat descriptors_1,descriptors_2;
 		
 		cv::Ptr<cv::FeatureDetector> detector = cv::cuda::ORB::create();
 		cv::Ptr<cv::DescriptorExtractor> descriptor = cv::cuda::ORB::create();
-		cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForece-Hamming");
+		cv::Ptr<cv::cuda::DescriptorMatcher> matcher = cv::cuda::DescriptorMatcher::createBFMatcher(cv::NORM_HAMMING);
+		//cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
 		
 		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 		detector -> detect(Mat_buffer[0],keypoints_1);
@@ -42,8 +47,23 @@ int main(int argc, char *argv[]){
 		descriptor -> compute(Mat_buffer[0],keypoints_1,descriptors_1);
 		descriptor -> compute(Mat_buffer[1],keypoints_2,descriptors_2);
 		std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-
-				
+		std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+		std::cout<<"extract ORB cost: "<<time_used.count()<<std::endl;
+		
+		std::vector<cv::DMatch> matches;
+		t1 = std::chrono::steady_clock::now();
+		matcher -> match(descriptors_1,descriptors_2,matches);
+		t2 = std::chrono::steady_clock::now();
+		time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);	
+		std::cout<<"match ORB cost: "<<time_used.count()<<std::endl;
+		
+		
+		auto min_max = std::minmax_element(matches.begin(),matches.end(),[](const cv::DMatch &m1, const cv::DMatch &m2){return m1.distance < m2.distance;});	
+		double min_dist = min_max.first -> distance;
+		double max_dist = min_max.second -> distance;
+		
+		//std::vector<cv::DMat> good_matches
+		
 		cv::Mat mat_plot;
 		Mat_buffer[0].download(mat_plot);
 		cv::imshow("Display_color",mat_plot);
